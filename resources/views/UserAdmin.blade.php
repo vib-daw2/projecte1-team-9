a22aca6eb1eab7343cc102da6c77fd76b4fdfef5
Carlos Carpio <darkyelan@gmail.com>
2023-09-25 14:24:05 +0200

    User admin view

--- resources/views/UserAdmin.blade.php
+++ resources/views/UserAdmin.blade.php
@@ -41,22 +41,22 @@
             </a>
             <div class="flex flex-col justify-center items-center gap-2">
                 <button
-                    class="py-2 relative group px-2 rounded-md text-black w-fit h-fit bg-white hover:bg-gray-100 border border-black hover:border-r-0 hover:rounded-r-none flex justify-between items-center gap-2">
+                    class="py-2 relative group px-2 rounded-md text-black w-fit h-fit bg-white hover:bg-gray-100 border border-black hover:border-l-0 hover:rounded-l-none flex justify-between items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/><path d="m15 5 4 4"/></svg>
-                    <div class="group-hover:block hidden absolute left-10 w-24 py-2 bg-gray-100 rounded-r-md border border-black border-l-0">Edit</div>
+                    <div class="group-hover:block hidden absolute right-10 w-24 py-2 bg-gray-100 rounded-l-md border border-black border-r-0">Edit</div>
                 </button>
                 <button
-                    class="py-2 relative group px-2 rounded-md text-white w-fit h-fit bg-gray-900 hover:rounded-r-none hover:bg-gray-900/90 flex justify-between items-center gap-2">
+                    class="py-2 relative group px-2 rounded-md text-white w-fit h-fit bg-gray-900 hover:rounded-l-none hover:bg-gray-900/90 flex justify-between items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                         stroke-linejoin="round" class="lucide lucide-circle-slash">
                         <line x1="9" x2="15" y1="15" y2="9" />
                         <circle cx="12" cy="12" r="10" />
                     </svg>
-                    <div class="group-hover:block hidden absolute left-10 w-24 py-2 bg-gray-900/90 text-white rounded-r-md ">Block</div>
+                    <div class="group-hover:block hidden absolute right-10 w-24 py-2 bg-gray-900/90 text-white rounded-l-md ">Block</div>
                 </button>
                 <button
-                    class="py-2 px-2 group relative rounded-md text-white w-fit h-fit bg-red-500 hover:bg-red-500/90 hover:rounded-r-none flex justify-between items-center gap-2">
+                    class="py-2 px-2 group relative rounded-md text-white w-fit h-fit bg-red-500 hover:bg-red-500/90 hover:rounded-l-none flex justify-between items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                         stroke-linejoin="round" class="lucide lucide-trash-2">
@@ -66,7 +66,7 @@
                         <line x1="10" x2="10" y1="11" y2="17" />
                         <line x1="14" x2="14" y1="11" y2="17" />
                     </svg>
-                    <div class="group-hover:block hidden absolute left-10 w-24 py-2 bg-red-500/90 rounded-r-md">Delete</div>
+                    <div class="group-hover:block hidden absolute right-10 w-24 py-2 bg-red-500/90 rounded-l-md">Delete</div>
                 </button>
             </div>
         </div>
