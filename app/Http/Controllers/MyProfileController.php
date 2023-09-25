4be21cdc912fdfd3dc291c3802d41b858bb127c8
A1bert04 <albertbru04@gmail.com>
2023-09-25 13:58:31 +0200

    /me routes controllers done

--- app/Http/Controllers/MyProfileController.php
+++ app/Http/Controllers/MyProfileController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MyProfileController extends Controller
 {
-    public function render(){
-        return view('MyProfile');
+    public function render()
+    {
+        $user_id = Auth::id();
+
+        $posts_count = DB::table('blogs')->where('user_id', $user_id)->where('status', 'published')->count();
+        $up_since = DB::table('users')->where('id', $user_id)->value('created_at');
+
+        $likes = DB::table('likes')
+            ->where('blog_id', DB::table('blogs')
+                ->where('user_id', $user_id)->value('id'))
+            ->where('liked', true)->count(); // Calculate the number of likes for the user's posts
+
+        $user = DB::table('users')->where('id', $user_id)->first();
+
+        return view('MyProfile', [
+            'posts_count' => $posts_count,
+            'up_since' => $up_since,
+            'likes' => $likes,
+            'email' => $user->email,
+            'username' => $user->username,
+        ]);
     }
 }
--- app/Http/Controllers/ProfilePostsController.php
+++ app/Http/Controllers/ProfilePostsController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ProfilePostsController extends Controller
 {
     public function render(){
-        return view('profile-posts');
+
+        $user_id = Auth::id();
+        $blogs = DB::table('blogs')->where('user_id', $user_id)->where('status', 'published')->get();
+
+        return view('profile-posts', [
+            'blogs' => $blogs,
+        ]);
     }
 }
