f784a414d6dd47c39498cd7740cd0afa1ea2aa72
A1bert04 <albertbru04@gmail.com>
2023-09-25 19:14:04 +0200

    404 and 403 error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+<div class="w-screen h-screen flex flex-col justify-center items-center text-2xl font-medium">
+<div class="text-9xl">403</div>
+    <div class="text-4xl">Forbidden</div>
+    <div class="text-2xl">You don't have permission to access this page.</div>
+    <a href="/" class="text-xl underline">Go back to home</a>
+</div>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+<div class="w-screen h-screen flex flex-col justify-center items-center text-2xl font-medium">
+    <div class="text-9xl">404</div>
+    <div class="text-4xl">Not Found</div>
+    <div class="text-2xl">The page you're looking for doesn't exist.</div>
+    <a href="/" class="text-xl underline">Go back to home</a>
+</div>
