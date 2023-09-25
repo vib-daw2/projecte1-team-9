457efeeb27967f9fc50b6fbf4a2d270f3c40c865
A1bert04 <albertbru04@gmail.com>
2023-09-25 19:14:24 +0200

    User admin policy

--- app/Http/Controllers/Admin/UserAdminController.php
+++ app/Http/Controllers/Admin/UserAdminController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Throwable;
 
 class UserAdminController extends Controller
 {
-    //
+    public function render(): Factory|\Illuminate\Foundation\Application|View|Redirector|Application|RedirectResponse
+    {
+        try {
+            $this->authorize('viewAny', User::class);
+        } catch (Throwable $th) {
+            abort(403, $th->getMessage());
+        }
 
-    public function render(){
-        return view('admin/users');
+        $users = User::all();
+
+        return view('admin/users', ['users' => $users]);
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\Blog;
+use App\Models\User;
 use App\Policies\BlogPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Blog::class => BlogPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
