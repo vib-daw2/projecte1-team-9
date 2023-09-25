c5800ffd42f1a930d4081bc3847ead2412297b59
Alumne Matí <darkyelan@gmail.com>
2023-09-25 10:57:36 +0200

    Fixed logout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -31,7 +31,7 @@
                 <path d="M20 21a8 8 0 1 0-16 0" />
             </svg>
         </x-navbutton>
-        <x-navbutton href="/logout">
+        {{-- <x-navbutton href="/logout">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="lucide lucide-log-out">
@@ -39,7 +39,8 @@
                 <polyline points="16 17 21 12 16 7" />
                 <line x1="21" x2="9" y1="12" y2="12" />
             </svg>
-        </x-navbutton>
+        </x-navbutton> --}}
+        <x-logout />
     @endauth
 
 </nav>
