cee6a80b706f6a6daa7ba7a1336681658637c241
Carlos Carpio <darkyelan@gmail.com>
2023-09-25 13:41:54 +0200

    Finished sidebar

--- resources/views/components/logout.blade.php
+++ resources/views/components/logout.blade.php
@@ -1,6 +1,7 @@
 <form method="POST" action="/logout" class="w-full">
     @csrf
-    <button class="hover:text-white hover:bg-red-500 w-2/3 py-2 rounded-md flex mx-auto justify-center items-center hover:rounded-r-none group relative">
+    <button
+        class="hover:text-white hover:bg-red-500 w-2/3 py-2 rounded-md flex mx-auto justify-center items-center hover:rounded-r-none group relative">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
             class="lucide lucide-log-out">
@@ -8,6 +9,8 @@
             <polyline points="16 17 21 12 16 7" />
             <line x1="21" x2="9" y1="12" y2="12" />
         </svg>
-        <div class="hidden group-hover:block absolute top-0 left-16 text-center bg-red-5<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-monitor"><rect width="20" height="14" x="2" y="3" rx="2"/><line x1="8" x2="16" y1="21" y2="21"/><line x1="12" x2="12" y1="17" y2="21"/></svg>00 text-white rounded-r-md  w-24 px-2 py-2">Log Out</div>
+        <div
+            class="hidden group-hover:block absolute top-0 left-16 text-center bg-red-500 text-white rounded-r-md  w-24 px-2 py-2">
+            Log Out</div>
     </button>
 </form>
