3c100d977124dd11998e2b9fd584b3edde92a5eb
A1bert04 <albertbru04@gmail.com>
2023-09-26 14:17:43 +0200

    Fixed like button style

--- resources/views/components/blog-like.blade.php
+++ resources/views/components/blog-like.blade.php
@@ -24,7 +24,7 @@
         form.submit();
     }
 </script>
-<form class="flex items-center gap-2" id="like-form-{{$id}}" method="POST" action="/blog/{{$id}}/like">
+<form class="flex items-center gap-2 mb-0" id="like-form-{{$id}}" method="POST" action="/blog/{{$id}}/like">
     @csrf
     <input type="hidden" name="action" value="">
     {{ $likes }}
