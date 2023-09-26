043b150ea2b23cb8f530be6956daa08425c3de4a
A1bert04 <albertbru04@gmail.com>
2023-09-26 12:09:17 +0200

    Returned home to previous version

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,25 +10,12 @@ class HomeController extends Controller
 {
     public function render()
     {
-        if (Auth::check()) {
-            $blogs = DB::table('blogs')
-                ->select('blogs.*', 'users.username', 'users.id as owner_id', 'liked')
-                ->join('users', 'users.id', '=', 'blogs.user_id')
-                ->where('blogs.status', '=', 'published')
-                ->leftJoin('likes', function ($join) {
-                    $join->on('likes.blog_id', '=', 'blogs.id')
-                        ->where('likes.user_id', '=', Auth::id());
-                })
-                ->orderBy('views', 'desc')
-                ->paginate(10);
-        } else {
-            $blogs = DB::table('blogs')
-                ->select('blogs.*', 'users.username', 'users.id as owner_id')
-                ->join('users', 'users.id', '=', 'blogs.user_id')
-                ->where('blogs.status', '=', 'published')
-                ->orderBy('views', 'desc')
-                ->paginate(10);
-        }
+        $blogs = DB::table('blogs')
+            ->select('blogs.*', 'users.username', 'users.id as owner_id')
+            ->join('users', 'users.id', '=', 'blogs.user_id')
+            ->where('blogs.status', '=', 'published')
+            ->orderBy('views', 'desc')
+            ->paginate(10);
 
         return view('home', [
             "blogs" => $blogs
