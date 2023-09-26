541606d944d5bd6d9bba7a3aea6401e7fc41497e
A1bert04 <albertbru04@gmail.com>
2023-09-26 12:19:25 +0200

    Changed likes

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\DB;
 
 class Blog extends Model
@@ -20,12 +22,12 @@ class Blog extends Model
         'published',
     ];
 
-    public function likes(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function likes(): HasMany
     {
         return $this->hasMany(Like::class);
     }
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
@@ -36,33 +38,41 @@ class Blog extends Model
      */
     public function liked(): bool|null
     {
-       $liked = DB::table('likes')
-           ->where('blog_id', $this->id)
-           ->where('user_id', auth()->id())
-           ->first();
-        return $liked->liked ?? null;
-    }
-
-    public function getLikesAndDislikes(): object
-    {
-        // Usamos raw para solo tener que hacer una query
-        return DB::table('likes')
-            ->select(DB::raw('SUM(liked = true) as likes, SUM(liked = false) as dislikes'))
+        $liked = DB::table('likes')
+            ->select('type')
             ->where('blog_id', $this->id)
+            ->where('user_id', auth()->id())
             ->first();
+        if ($liked->type === 'like') {
+            return true;
+        } else if ($liked->type === 'dislike') {
+            return false;
+        } else {
+            return null;
+        }
     }
 
-    /**
-     * Function to validate the blog
-     * @return string[]
-     */
-    public static function validate(): array
-    {
-        return [
-            'title' => 'required|min:3|max:255',
-            'subtitle' => 'required|min:3|max:255',
-            'body' => 'required|min:3',
-            'status' => 'required|in:draft,published'
-        ];
+        public function getLikesAndDislikes(): object
+        {
+            // Usamos raw para solo tener que hacer una query
+            return DB::table('likes')
+                ->select(DB::raw('SUM(CASE WHEN type = "like" THEN 1 ELSE 0 END) as likes'), DB::raw('SUM(CASE WHEN type = "dislike" THEN 1 ELSE 0 END) as dislikes'))
+                ->where('blog_id', $this->id)
+                ->first();
+        }
+
+        /**
+         * Function to validate the blog
+         * @return string[]
+         */
+        public
+        static function validate(): array
+        {
+            return [
+                'title' => 'required|min:3|max:255',
+                'subtitle' => 'required|min:3|max:255',
+                'body' => 'required|min:3',
+                'status' => 'required|in:draft,published'
+            ];
+        }
     }
-}
