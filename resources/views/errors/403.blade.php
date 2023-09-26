afe781705d380bb56abd158fcf20f97ab5fb49a8
Carlos Carpio <darkyelan@gmail.com>
2023-09-26 10:07:23 +0200

    Finished error messages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,9 @@
 @extends('layout')
-<div class="w-screen h-screen flex flex-col justify-center items-center text-2xl font-medium">
-<div class="text-9xl">403</div>
-    <div class="text-4xl">Forbidden</div>
-    <div class="text-2xl">You don't have permission to access this page.</div>
-    <a href="/" class="text-xl underline">Go back to home</a>
+<div class="w-screen min-h-screen flex flex-col justify-center items-center gap-4">
+    <div>
+<div class="text-9xl font-black">403</div>
+    <div class="text-4xl text-center">Forbidden</div>
+    </div>
+    <div class="text-2xl font-light">You don't have permission to access this page.</div>
+    <a href="/" class="text-xl underline font-light underline-offset-2">Go back to home</a>
 </div>
