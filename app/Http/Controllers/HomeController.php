1c48ee3785fec35bd84d80b046d5c24eed712495
A1bert04 <albertbru04@gmail.com>
2023-09-26 11:59:00 +0200

    Made home return if the blog was liked or not

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,13 @@ class HomeController extends Controller
     public function render(Request $request)
     {
         $blogs = DB::table('blogs')
-            ->select('blogs.*', 'users.username', 'users.id as owner_id')
+            ->select('blogs.*', 'users.username', 'users.id as owner_id', 'liked')
             ->join('users', 'users.id', '=', 'blogs.user_id')
             ->where('blogs.status', '=', 'published')
+            ->leftJoin('likes', function ($join) use ($request) {
+                $join->on('likes.blog_id', '=', 'blogs.id')
+                    ->where('likes.user_id', '=', $request->session()->get('user')->id);
+            })
             ->orderBy('views', 'desc')
             ->paginate(10);
 
