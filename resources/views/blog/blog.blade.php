a80c13f9d409fe9a4e75e8b7159c7b18c568d58c
A1bert04 <albertbru04@gmail.com>
2023-09-26 09:28:36 +0200

    Fixed blog title overflow

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -2,7 +2,7 @@
 <div class="w-full min-h-screen flex justify-start flex-col items-center px-12 pt-12">
     <div class="max-w-3xl mt-4 w-full mx-auto flex flex-col justify-start items-start gap-3">
         <div class="w-full flex justify-between items-start py-2">
-        <div class="text-5xl overflow-clip">{{ $blog->title }}</div>
+        <div class="text-5xl">{{ $blog->title }}</div>
         @if ($blog->status == "draft")
                 <div class="rounded-md bg-gray-900 text-white px-2 py-1 w-fit font-medium text-sm">DRAFT</div>
             @endif
