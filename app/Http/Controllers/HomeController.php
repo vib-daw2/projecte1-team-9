2a5c18f82f4cf7132b3f1e5f9aa05d220f2910c3
A1bert04 <albertbru04@gmail.com>
2023-09-26 12:01:19 +0200

    Fixed home when user is not logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,25 @@ class HomeController extends Controller
 {
     public function render(Request $request)
     {
-        $blogs = DB::table('blogs')
-            ->select('blogs.*', 'users.username', 'users.id as owner_id', 'liked')
-            ->join('users', 'users.id', '=', 'blogs.user_id')
-            ->where('blogs.status', '=', 'published')
-            ->leftJoin('likes', function ($join) use ($request) {
-                $join->on('likes.blog_id', '=', 'blogs.id')
-                    ->where('likes.user_id', '=', Auth::id());
-            })
-            ->orderBy('views', 'desc')
-            ->paginate(10);
+        if (Auth::check()) {
+            $blogs = DB::table('blogs')
+                ->select('blogs.*', 'users.username', 'users.id as owner_id', 'liked')
+                ->join('users', 'users.id', '=', 'blogs.user_id')
+                ->where('blogs.status', '=', 'published')
+                ->leftJoin('likes', function ($join) use ($request) {
+                    $join->on('likes.blog_id', '=', 'blogs.id')
+                        ->where('likes.user_id', '=', Auth::id());
+                })
+                ->orderBy('views', 'desc')
+                ->paginate(10);
+        } else {
+            $blogs = DB::table('blogs')
+                ->select('blogs.*', 'users.username', 'users.id as owner_id')
+                ->join('users', 'users.id', '=', 'blogs.user_id')
+                ->where('blogs.status', '=', 'published')
+                ->orderBy('views', 'desc')
+                ->paginate(10);
+        }
 
         return view('home', [
             "blogs" => $blogs
