d929897903c79c7903462317103c777955ebb67d
A1bert04 <albertbru04@gmail.com>
2023-09-26 12:03:12 +0200

    Removed unused function param

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,14 +8,14 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function render(Request $request)
+    public function render()
     {
         if (Auth::check()) {
             $blogs = DB::table('blogs')
                 ->select('blogs.*', 'users.username', 'users.id as owner_id', 'liked')
                 ->join('users', 'users.id', '=', 'blogs.user_id')
                 ->where('blogs.status', '=', 'published')
-                ->leftJoin('likes', function ($join) use ($request) {
+                ->leftJoin('likes', function ($join) {
                     $join->on('likes.blog_id', '=', 'blogs.id')
                         ->where('likes.user_id', '=', Auth::id());
                 })
