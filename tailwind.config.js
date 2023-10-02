6aca303069c27a86059a9ce2257d96e5ffb29c38
Carlos Carpio <darkyelan@gmail.com>
2023-10-02 13:42:21 +0200

    Added toast component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ export default {
     "./resources/**/*.blade.php"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        input: {
+          '0%': { transform: 'translateY(0px)' },
+          '100%': { transform: 'translateY(-25px)' },
+        },
+        toastin: {
+          '0%' : { transform: 'translateY(-120%)' },
+          '25%': { transform: 'translateY(0)' },
+          '50%' : { transform: 'translateY(0)' },
+          '75%': { transform: 'translateY(0)' },
+          '100%' : { transform: 'translateY(-120%)' },
+        },
+        toastout: {
+          '0%' : { transform: 'translateY(0)' },
+          '100%' : { transform: 'translateY(-120%)' },
+        }
+      },
+      animation: {
+        input: 'input 0.5s ease-in-out forwards',
+        noinput: 'input 0.5s ease-in-out reverse forwards',
+        toastin: 'toastin 6.5s forwards ease-in-out',
+        toastout: 'toastout 0.5s forwards ease-in-out'
+      }
+    },
   },
   plugins: [],
 }
