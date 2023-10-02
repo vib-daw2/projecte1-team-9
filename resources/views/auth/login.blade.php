81ce18b5c02ce761819f0da210d9818ad835b6ac
Carlos Carpio <darkyelan@gmail.com>
2023-10-02 12:37:34 +0200

    Added auth icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,80 @@
 @extends('layout')
 <div class="w-full flex justify-center items-center min-h-screen bg-gray-50 overflow-hidden">
-    <div class="rounded-lg border h-fit shadow-lg w-full max-w-md p-8 bg-white">
-        <div class="text-2xl font-medium">Log In</div>
-        {{-- <div class="w-full mt-4 flex justify-between items-center gap-4">
-            <div class="w-12 h-12 flex justify-center items-center rounded-md ring-1 ring-black">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+    <div class="w-full flex justify-center items-start drop-shadow-lg">
+        <div class="w-16 h-32 bg-white rounded-l-md shadow-lg border flex flex-col">
+            <div
+                class="w-16 h-16 group flex justify-center hover:bg-gray-200 items-center border-b border-b-black rounded-tl-md">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-github">
+                    <path
+                        d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                    <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
             </div>
-        </div> --}}
-        <form class="w-full mt-4 flex flex-col gap-4 " method="POST">
-            @csrf
-            <div class="w-full">
-                <label class="block">Username</label>
-                <input type="text" name="username" id="username" class="px-2 py-1 rounded-lg ring-1 ring-black w-full">
-            </div>
-            <div class="w-full">
-                <label class="block">Password</label>
-                <input type="password" name="password" id="password"
-                    class="px-2 py-1 ring-black rounded-lg ring-1 w-full">
-            </div>
-            @error('username')
-            <div>
-                <div class="text-red-500 text-sm mt-1">{{$message}}</div>
-            </div>
-            @enderror
-            <div class="flex justify-between items-center">
-                <div>
-                    <input type="checkbox" name="remember" id="remember">
-                    <label for="remember" class="select-none">Remember me</label>
-                </div>
-                <a class="text-red-500 underline underline-offset-4 hover:text-red-600" href="#">Forgot my
-                    password</a>
-            </div>
-            <input type="submit" value="Log In"
-                class="w-full bg-gray-900 hover:bg-gray-900/90 font-medium text-white py-2 rounded-lg">
-        </form>
-        <div class="w-full h-4 mt-4 flex justify-center items-center">
-            <div class="w-5/12">
-                <div class="h-1/2 border-b"></div>
-            </div>
-            <div class="w-2/12 text-center">or</div>
-            <div class="w-5/12">
-                <div class="h-1/2 border-b"></div>
+            <div
+                class="w-16 h-16 hover:bg-gray-200 flex justify-center items-center rounded-bl-md group">
+
+                <svg class="" xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 0 24 24" width="28">
+                    <path
+                        d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                        fill="#4285F4" />
+                    <path
+                        d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                        fill="#34A853" />
+                    <path
+                        d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                        fill="#FBBC05" />
+                    <path
+                        d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                        fill="#EA4335" />
+                    <path d="M1 1h22v22H1z" fill="none" />
+                    
+                </svg>
             </div>
         </div>
-        <a href="/signup"
-            class="w-full mt-4 block bg-white hover:bg-gray-50 font-medium text-black ring-1 ring-black py-2 rounded-lg text-center">Sign
-            up</a>
+        <div class="rounded-lg h-fit w-full max-w-md p-8 bg-white border rounded-tl-none">
+            <div class="text-2xl font-medium">Log In</div>
+            <form class="w-full mt-4 flex flex-col gap-4 " method="POST">
+                @csrf
+                <div class="w-full">
+                    <label class="block">Username</label>
+                    <input type="text" name="username" id="username"
+                        class="px-2 py-1 rounded-lg ring-1 ring-black w-full">
+                </div>
+                <div class="w-full">
+                    <label class="block">Password</label>
+                    <input type="password" name="password" id="password"
+                        class="px-2 py-1 ring-black rounded-lg ring-1 w-full">
+                </div>
+                @error('username')
+                    <div>
+                        <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
+                    </div>
+                @enderror
+                <div class="flex justify-between items-center">
+                    <div>
+                        <input type="checkbox" name="remember" id="remember">
+                        <label for="remember" class="select-none">Remember me</label>
+                    </div>
+                    <a class="text-red-500 underline underline-offset-4 hover:text-red-600" href="#">Forgot my
+                        password</a>
+                </div>
+                <input type="submit" value="Log In"
+                    class="w-full bg-gray-900 hover:bg-gray-900/90 font-medium text-white py-2 rounded-lg">
+            </form>
+            <div class="w-full h-4 mt-4 flex justify-center items-center">
+                <div class="w-5/12">
+                    <div class="h-1/2 border-b"></div>
+                </div>
+                <div class="w-2/12 text-center">or</div>
+                <div class="w-5/12">
+                    <div class="h-1/2 border-b"></div>
+                </div>
+            </div>
+            <a href="/signup"
+                class="w-full mt-4 block bg-white hover:bg-gray-50 font-medium text-black ring-1 ring-black py-2 rounded-lg text-center">Sign
+                up</a>
+        </div>
     </div>
 </div>
