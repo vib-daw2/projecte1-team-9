5a191f1ff577780090917ebf8ecb8a4959011f5f
A1bert04 <albertbru04@gmail.com>
2023-10-02 09:05:26 +0200

    Refactor web.php

--- routes/web.php
+++ routes/web.php
@@ -28,41 +28,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'render']);
+Route::get('/', [HomeController::class, 'render']); // Home view
 
-Route::get('/login', [LoginController::class, 'render']);
-Route::post('/login', [LoginController::class, 'login']);
+/*
+ * AUTHENTICATION
+ *
+ * All the routes that are related to the authentication
+ * Login, signup, logout
+ * */
+Route::get('/login', [LoginController::class, 'render']); // Login view
+Route::post('/login', [LoginController::class, 'login']); // Login action
+Route::get('/signup', [SignupController::class, 'render']); // Signup view
+Route::post('/signup', [SignupController::class, 'signup']); // Signup action
+Route::post('/logout', [LogoutController::class, 'logout']); // Logout action
 
-Route::get('/signup', [SignupController::class, 'render']);
-Route::post('/signup', [SignupController::class, 'signup']);
 
-Route::post('/logout', [LogoutController::class, 'logout']);
+/*
+ * BLOGS
+ *
+ * All the routes that are related to the blogs
+ * */
+Route::get('/blog/{id}', [BlogController::class, 'render']); // Blog view
+Route::get('/blog/new', [NewBlogController::class, 'render']); // New blog view
+Route::post('/blog/new', [NewBlogController::class, 'create']); // New blog action
+Route::get('/blog/{id}/edit', [EditBlogController::class, 'render']); // Edit blog view
+Route::post('/blog/{id}/edit', [EditBlogController::class, 'edit']); // Edit blog action
+Route::post('/blog/{id}/like', [LikeController::class, 'like']); // Like/dislike blog action
 
-Route::get('/blog/new', [NewBlogController::class, 'render']);
-Route::post('/blog/new', [NewBlogController::class, 'create']);
 
-Route::get('/blog/{id}', [BlogController::class, 'render']);
+/*
+ * PROFILES
+ *
+ * All the routes that are related to the profiles
+ * */
+Route::get('/user/{id}', [ProfileController::class, 'render']); // Profile view
+Route::get('/me', [MyProfileController::class, 'render']); // My profile view / Edit profile view
+Route::get('/me/likes', [MyLikesController::class, 'render']); // View blogs that I liked
+Route::get('/me/posts', [ProfilePostsController::class, 'render']); // View my posts
 
-Route::get('/blog/{id}/edit', [EditBlogController::class, 'render']);
-Route::post('/blog/{id}/edit', [EditBlogController::class, 'edit']);
+/*
+ * EDIT PROFILE
+ *
+ * All the routes that are related to the profile editing
+ * Change password, edit profile, delete profile
+ * */
+Route::post('/me', [MyProfileController::class, 'edit']); // Edit profile
+Route::get('/me/password', [ChangePasswordController::class, 'render']); // Change password view
+Route::post('/me/password', [ChangePasswordController::class, 'changePassword']); // Change password action
 
-Route::post('/blog/{id}/like', [LikeController::class, 'like']);
 
-Route::get('/me', [MyProfileController::class, 'render']);
-Route::post('/me', [MyProfileController::class, 'edit']);
-
-Route::get('/me/posts', [ProfilePostsController::class, 'render']);
-
-Route::get('/me/likes', [MyLikesController::class, 'render']);
-
-Route::get('/me/password', [ChangePasswordController::class, 'render']);
-Route::post('/me/password', [ChangePasswordController::class, 'changePassword']);
-
-Route::get('/user/{id}', [ProfileController::class, 'render']);
-
-Route::get('/admin/users', [UserController::class, 'render']);
-Route::get('/admin/users/{id}', [EditUserController::class, 'render']);
-Route::post('/admin/users/{id}/delete', [UserController::class, 'delete']);
-
-Route::get('/admin/users/{id}/edit', [EditUserController::class, 'render']);
-Route::post('/admin/users/{id}/edit', [EditUserController::class, 'edit']);
+/*
+ * ADMIN PANEL
+ *
+ * All the routes that are related to the admin panel
+ * */
+Route::get('/admin/users', [UserController::class, 'render']); // List all users
+Route::post('/admin/users/{id}/delete', [UserController::class, 'delete']); // Delete a user action
+Route::get('/admin/users/{id}/edit', [EditUserController::class, 'render']); // Edit a user view
+Route::post('/admin/users/{id}/edit', [EditUserController::class, 'edit']); // Edit a user action
