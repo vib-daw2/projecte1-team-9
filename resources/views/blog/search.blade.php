e96e36c0a73317b1a88b81414ec7aa3aa021bf41
Carlos Carpio <darkyelan@gmail.com>
2023-10-03 12:50:37 +0200

    Fixed follow and logout buttons

--- resources/views/blog/search.blade.php
+++ resources/views/blog/search.blade.php
@@ -59,10 +59,30 @@
                         </div>
                         @auth
                         @if(Auth::id() != $user->id)
-                        <button class="w-36 font-medium flex justify-center items-center px-2 gap-2 py-2 bg-gray-900 hover:bg-gray-900/90 text-white rounded-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-plus-2"><path d="M14 19a6 6 0 0 0-12 0"/><circle cx="8" cy="9" r="4"/><line x1="19" x2="19" y1="8" y2="14"/><line x1="22" x2="16" y1="11" y2="11"/></svg>
-                            Follow
-                        </button>
+                        <form method="POST" action="/follow/{{$user->id}}"
+                            class="flex flex-row gap-2 w-1/3 items-center justify-center">
+                          @csrf
+                          @php
+                              $following = DB::table('follows')
+                                ->select('follows.*')
+                                ->where('follows.follower_id', '=', Auth::id())
+                                ->where('follows.followee_id', '=', $user->id)
+                                ->first();
+                          @endphp
+                            @if($following)
+                            <button class="w-36 group font-medium flex justify-center items-center px-2 gap-2 py-1 bg-gray-900 text-white hover:bg-red-600 rounded-md">
+                                <svg class="group-hover:hidden block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-check-2"><path d="M14 19a6 6 0 0 0-12 0"/><circle cx="8" cy="9" r="4"/><polyline points="16 11 18 13 22 9"/></svg>
+                                <span class="group-hover:hidden block">Following</span>
+                                <svg class="group-hover:block hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-x-2"><path d="M14 19a6 6 0 0 0-12 0"/><circle cx="8" cy="9" r="4"/><line x1="17" x2="22" y1="8" y2="13"/><line x1="22" x2="17" y1="8" y2="13"/></svg>
+                                <span class="group-hover:block hidden">Unfollow</span>
+                            </button>
+                            @else
+                            <button class="w-36 font-medium flex justify-center items-center px-2 gap-2 py-1 bg-white hover:bg-gray-200 ring-1 ring-black rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-plus-2"><path d="M14 19a6 6 0 0 0-12 0"/><circle cx="8" cy="9" r="4"/><line x1="19" x2="19" y1="8" y2="14"/><line x1="22" x2="16" y1="11" y2="11"/></svg>
+                                Follow
+                            </button>
+                        </form>
+                        @endif
                         @else
                         <div class="w-36"></div>
                         @endif
--- resources/views/components/logout.blade.php
+++ resources/views/components/logout.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="/logout" class="w-full h-fit lg:my-0 my-auto">
     @csrf
     <button
-        class="hover:text-white hover:bg-red-500 w-2/3 py-2 my-auto rounded-md flex mx-auto justify-center items-center lg:hover:rounded-r-none group relative">
+        class="hover:text-white hover:bg-red-500 w-2/3 py-2 lg:my-0 my-auto rounded-md flex mx-auto justify-center items-center lg:hover:rounded-r-none group relative">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
             class="lucide lucide-log-out">
