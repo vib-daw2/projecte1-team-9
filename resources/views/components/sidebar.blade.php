276007e58a15c1266c73b280791e6dc7bf4f36bd
Carlos Carpio <darkyelan@gmail.com>
2023-10-03 10:33:19 +0200

    Fixed sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -23,7 +23,8 @@
                 <line x1="15" x2="3" y1="12" y2="12" />
             </svg>
         </x-navbutton>
-    @endguest
+        @endguest
+        <x-navbutton href="/search" title="Search"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg></x-navbutton>
     @auth
         <x-navbutton href="/me" title="Profile">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
