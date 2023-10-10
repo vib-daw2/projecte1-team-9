25506286b7c6d115cd7fa0835af9414ee06e8a39
A1bert04 <albertbru04@gmail.com>
2023-10-10 09:12:04 +0200

    Moved child comments to another table

--- database/migrations/2023_10_10_071103_create_comments_childs.php
+++ database/migrations/2023_10_10_071103_create_comments_childs.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('comments_childs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->text('body');
             $table->foreignId('blog_id')->constrained('blogs')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('parent_id')->constrained('comments')->onDelete('cascade');
         });
     }
 
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('comments_childs');
     }
 };
